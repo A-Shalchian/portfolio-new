06ad5dac920f498fd5dbe442496a6bd3c3a0d4a5
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-04-25 04:03:55 -0400

    Add Skills component to Home layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import { Profile } from "../homeSections/Profile";
 import { Education } from "../homeSections/Education";
+import { Skills } from "../homeSections/Skills";
 
 export const Home = () => {
   return (
     <div>
       <Profile />
       <Education />
+      <Skills />
     </div>
   );
 };
